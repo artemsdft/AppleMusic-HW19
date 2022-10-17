27ba63f6c900b19ff742ecbbc06e84d6926f31c3
Artem Derbin <artemsdft@mail.ru>
2022-10-17 16:09:13 +0300

    Initial Commit

---
+++ AppleMusic HW19/AppleMusic_HW19App.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_HW19App.swift
+//  AppleMusic HW19
+//
+//  Created by Артем Дербин on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_HW19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
